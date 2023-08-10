9d49723b58cf3783f19adba7397235146c2b2113
thienmy0 <111404222+thienmy0@users.noreply.github.com>
2023-08-10 14:45:12 +0800

    Instruction Code

--- Atolie/Assets/Scripts/Instruction/ContinueButton.cs
+++ Atolie/Assets/Scripts/Instruction/ContinueButton.cs
@@ -7,6 +7,7 @@ public class ContinueButton : MonoBehaviour
 {
     public Image image;
 
+    //Destroys current instruction screen to reveal next set of instructions
     public void ContinueInstruction()
     {
         Destroy(image.gameObject);
